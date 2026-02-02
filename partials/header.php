8f26e1b042f9e1665815cfc9fec390d850923114
Jeremy Turowetz <j.turowetz@gmail.com>
2026-02-01 20:18:28 -0500

    make it simple stupid

---
+++ partials/header.php
@@ -0,0 +1,22 @@
+<?php
+$page = $_SERVER['PHP_SELF'];
+$sec = "43200";
+?>
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <meta http-equiv="refresh" content="<?php echo $sec?>;URL='<?php echo $page?>'">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <title><?php echo $pageTitle ?? 'WTS Home Tab'; ?></title>
+        <!-- goto https://www.textfixer.com/html/html-character-encoding.php to encode accents -->
+        <link rel="stylesheet" href="assets/style.css">
+        <script>
+         (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+          ga('create', 'UA-6137292-1', 'standardpatiodoors.com');
+          ga('send', 'pageview');
+        </script>
+    </head>
+    <body>
--- wts-rona-en.php
+++ wts-rona-en.php
@@ -1,81 +1,9 @@
 <?php
-$page = $_SERVER['PHP_SELF'];
-$sec = "43200";
-?><!DOCTYPE HTML>
-<html>
-    <head>
-    	<meta http-equiv="refresh" content="<?php echo $sec?>;URL='<?php echo $page?>'">
-    	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
- 		 <title>WTS RONA Home Tab EN</title>
- 		<!-- goto https://www.textfixer.com/html/html-character-encoding.php to encode accents -->
-                <style>
-            #container, #left, #right{
-                margin: 0px; 
-                padding: 30px; 
-                border: 0px;
-            }
-
-            #left{
-                max-width: 100%; 
-                float: left;
-            }
-
-            #right{
-                max-width: 100%; 
-                float: left;
-            }
-
-            .centerimage {
-             text-align:center;
-             display:block;
-            }
-
-            .leftalign {
-             text-align:leftalign;
-             display:block;
-            }
-
-            .redtext{
-                color:red;
-            }
-
-            .bluetext{
-                color:#00BFFF;
-            }
-
-            body {
-             margin: 0;
-             font-family: 'Helvetica', 'Arial', sans-serif;
-            }
-
-            table {
-             font-family: arial, sans-serif;
-             border-collapse: collapse;
-             width: 90%;
-            }
-
-            td, th {
-             border: 1px solid #dddddd;
-             text-align: left;
-             padding: 8px;
-            }
-
-            tr:nth-child(even) {
-             background-color: #dddddd;
-            }
-        </style>
-        <script>
-         (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-          })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-          ga('create', 'UA-6137292-1', 'standardpatiodoors.com');
-          ga('send', 'pageview');
-        </script>
-    </head>
-    <body>
+$pageTitle = 'WTS RONA Home Tab EN';
+include 'partials/header.php';
+?>
         <div class="centerimage">
-            <a href="https://standarddoors.com/" target="_blank"><img src="/img/logos/LogoStandardColourEN2024.png"></a>
+            <a href="https://standarddoors.com/" target="_blank"><img src="assets/logos/LogoStandardColourEN2024.png"></a>
             <br>
             <br>
         </div>
@@ -106,5 +34,4 @@ $sec = "43200";
                     <h3>The entire team at STANDARD would like to thank you for your continued support and wish you Happy Holidays and all the best for 2026!</h3>
                 </div>';
         ?>
-	</body>
-</html>
+<?php include 'partials/footer.php'; ?>
